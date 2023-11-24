d092f0524002c34a98bc46a6559808152a76889e
${nei1d0r} <${neil.davis02@bbc.co.uk}>
2023-11-24 14:18:10 +0000

    save game: removed parsed data and added to gitignore

---
+++ lesson-1/node/data-set-generator.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+const { createCanvas } = require ('canvas');
+
+const draw = require('../common/draw');
+const constants = require('../common/constants');
+const utils = require('../common/utils');
+
+const canvas = createCanvas(400, 400)
+const ctx = canvas.getContext('2d')
+
+const generateImageFile = (outFile, paths) => {
+  ctx.clearRect(0,0,canvas.width, canvas.height)
+  draw.paths(ctx, paths)
+  const buffer = canvas.toBuffer('image/png')
+  fs.writeFileSync(outFile, buffer)
+}
+
+
+const fileNames = fs.readdirSync(constants.RAW_DIR);
+const samples = []
+let id = 1
+
+fileNames.forEach((fileName) => {
+  const content = fs.readFileSync(`${constants.RAW_DIR}/${fileName}`)
+  const { session, student, drawings } = JSON.parse(content)
+  for (let label in drawings) {
+    samples.push({
+      id,
+      label,
+      student_name: student,
+      session_id: session
+    })
+
+    const paths = drawings[label]
+    fs.writeFileSync(`${constants.JSON_DIR}/${id}.json`, JSON.stringify(paths))
+
+    generateImageFile(`${constants.IMG_DIR}/${id}.png`, paths)
+
+    utils.printProgress(id, (fileNames.length * 8))
+    id ++
+  }
+})
+
+fs.writeFileSync(constants.SAMPLES, JSON.stringify(samples))
+fs.writeFileSync(constants.SAMPLES_JS, `const samples = ${JSON.stringify(samples)};`)
